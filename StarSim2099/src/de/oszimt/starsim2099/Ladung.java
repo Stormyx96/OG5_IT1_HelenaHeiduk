fcab889b650eb52997f015313a2e1001fd4384c9
user <user@192.168.34.182>
2026-09-17 11:08:58 +0200

    attribute und getter/setter

--- StarSim2099/src/de/oszimt/starsim2099/Ladung.java
+++ StarSim2099/src/de/oszimt/starsim2099/Ladung.java
@@ -9,12 +9,54 @@ package de.oszimt.starsim2099;
 public class Ladung {
 
 	// Attribute
+private String typ;
+private int masse;
+private double posX;
+private double posY;
 
 	// Methoden
+public Ladung (String typ, int masse, double posX, double posY) {
+
+	
+}
+
 
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] ladungShape = { { '/', 'X', '\\' }, { '|', 'X', '|' }, { '\\', 'X', '/' } };
 		return ladungShape;
 	}
+
+public String getTyp() {
+	return this.typ;
 }
+public void setTyp(String neuTyp) {
+	this.typ = neuTyp;
+}
+
+public int getMasse() {
+	return this.masse;
+}
+public void setMasse(int neuMasse) {
+	this.masse = neuMasse;
+}
+
+public double getPosX() {
+	return this.PosX;
+}
+public void setPosX(double neuPosX) {
+	this.posX = neuPosX;
+}
+
+public double getPosY() {
+	return this.PosY;
+}
+public void setPosY(double neuPosY) {
+	this.posY = neuPosY;
+}
+
+
+
+
+}
+
